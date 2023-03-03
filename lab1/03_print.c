9bf9318db746d6f1093d4497954c80c1803212a6
xumingyukou <gaodaiwei2@gmail.com>
2023-03-03 20:41:44 +0800

    nothing

--- lab1/03_print.c
+++ lab1/03_print.c
@@ -6,10 +6,17 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include<stdlib.h>
 
 #define PPID_LINE 7
 
-int main()
+struct process {
+    char* name;
+    int pid;
+    int ppid;
+}processes[100];
+
+int getPID()
 {
 	DIR *dp;
 	DIR *subdp;
@@ -25,6 +32,7 @@ int main()
 	if((dp = opendir(dirname)) == NULL)
 		printf("Can't open %s\n", dirname);
 
+    int i = 0;
 	while((dirp = readdir(dp)) != NULL) {
         if(dirp -> d_name[0] >= '0' && dirp -> d_name[0] <= '9') {
 
@@ -48,9 +56,34 @@ int main()
 			
  	       	printf("\n--------------------------------\n");
 
+            processes[i].name = (char*)malloc(sizeof(name)+1);
+            strcpy(processes[i].name, name);
+            processes[i].pid = pid;
+            processes[i].ppid = ppid;
+
+            i++;
         }
     }
 
 	closedir(dp);
-	return 0;
+	return i;
+}
+
+void print() {
+    
+}
+
+int main() {
+    int totalNum = getPID();
+    printf("totalNum = %d\n", totalNum);
+
+    for(int i = 0; i < totalNum; i++)
+    {
+        printf("name: %s, pid: %d, ppid: %d\n", processes[i].name, processes[i].pid, processes[i].ppid);
+        printf("============================\n");
+    }
+
+
+
+    return 0;
 }
